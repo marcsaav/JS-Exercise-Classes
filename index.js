f54ebb43f12d444507fab0375c567a9191586979
Marcos Saavedra <marcsaav96@gmail.com>
2020-09-17 15:25:58 -0400

    Finished task 2

--- index.js
+++ index.js
@@ -78,7 +78,26 @@ class Person {
 */
 
 class Car {
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
 
+  fill(gallons) {
+    this.tank += gallons;
+  }
+
+  drive(distance) {
+    this.tank = this.tank - (distance / this.milesPerGallon);
+    if (this.tank <= 0) {
+      this.odometer = distance + (this.tank*this.milesPerGallon);
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    }
+    this.odometer = this.odometer + distance;
+  }
 }
 
 /*
